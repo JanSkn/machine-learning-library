d37a9feffd61bf7ac7189ea1a83dca9b68578dd9
Jan Skowron <jan.a.skowron@gmail.com>
2023-09-23 15:00:48 +0200

    added example of network evaluation

--- src/examples/mnist_predict.py
+++ src/examples/mnist_predict.py
@@ -7,7 +7,7 @@ sys.path.append(parent)
 
 import numpy as np
 from keras.datasets import mnist
-from network import predict, load
+from network import predict, load, evaluate
 from mnist_train import adjust_data
 
 network = load("mnist.pkl")
@@ -16,8 +16,15 @@ network = load("mnist.pkl")
 
 x_test, y_test = adjust_data(x_test, y_test, 20)
 
+x_data = [] 
+y_data = []
+
 for x, y in zip(x_test, y_test):
     output = predict(x, network)
+    x_data.append(np.argmax(output))
+    y_data.append(np.argmax(y))
     # argmax returns index of the highest value, thus returns the number with the highest prediction
     print(f"Prediction: {np.argmax(output)}, Real: {np.argmax(y)}")
 
+evaluation_data = (np.array(x_data).T, np.array(y_data).T)
+print(evaluate(evaluation_data[0], evaluation_data[1]))
