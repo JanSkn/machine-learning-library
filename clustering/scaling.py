140ecde2297bc87d1b930f587ed569f9bfc5a10b
Kai <kai.werk@gmx.de>
2023-10-21 11:19:59 +0200

    implemented feature scaling for clustering

---
+++ clustering/scaling.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class Scaling:
+    @staticmethod
+    def min_max_normalization(data: np.ndarray) -> np.ndarray:
+        """
+        Normalizes Data in a range from 0 to 1 using minx-max Value of every Feature. 
+        Wir machen das damit große im Sinne von hoher Wert Features unsere 
+        Ähnlichkeits bzw Distanzberechnungen nicht dominieren.
+        """
+        #each row is a data point, each column symbolizes a feature
+        #we need to find min and max for every feature
+        min = np.amin(data, axis=0)
+        max = np.amax(data, axis=0)
+        normalized_data = np.ndarray(shape=data.shape)
+
+        for (data_point, feature), value in np.ndenumerate(data):
+            normalized_data[data_point, feature] = (value-min[feature])/(max[feature]-min[feature])
+
+        return normalized_data
+    
+    @staticmethod
+    def z_normalization(data: np.ndarray) -> np.ndarray:
+        """
+        Normalizes Data in a range from 0 to 1 using Z-Scores.
+        Wir machen das damit große im Sinne von hoher Wert Features unsere 
+        Ähnlichkeits bzw Distanzberechnungen nicht dominieren.
+        """
+        #each row is a data point, each column symbolizes a feature
+        #we need to find mean and standart derivation for every feature
+        mean = np.mean(data, axis=0)
+        std = np.std(data, axis=0)
+        normalized_data = np.ndarray(shape=data.shape)
+
+        for (data_point, feature), value in np.ndenumerate(data):
+            normalized_data[data_point, feature] = (value-mean[feature])/(std[feature])
+
+        return normalized_data
+
