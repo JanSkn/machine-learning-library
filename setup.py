4e52dad4a64d8be667d5a1b0dd581f3aee963f0d
Jan Skowron <jan.a.skowron@gmail.com>
2023-11-12 18:32:43 +0100

    updated requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
 from setuptools import setup, find_packages
 
+"""
+pylearn
+=======
+
+pylearn is a Python machine learning library that provides implementations of various machine learning algorithms, 
+including neural networks, classification, and clustering.
+
+Installation
+------------
+
+You can (hopefully soon) install pylearn using pip:
+
+.. code-block:: bash
+
+   pip install pylearn
+
+Documentation
+-------------
+
+The official documentation can be found at: TODO
+
+License
+-------
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+"""
+
+
 # --- version ---
 # MAJOR.MINOR.PATCH
 # MAJOR for backwards incompatible changes.
 # MINOR for backwards-compatible new functions.
 # PATCH for backwards-compatible error corrections.
 
-with open('requirements.txt') as f:
+with open('docs/requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
