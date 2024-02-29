a5c993b8e92361a18c701e62658d66880c995fe3
JanSkn <jan.a.skowron@gmail.com>
2024-02-29 12:48:28 +0000

    added mnb

--- pylearn/classification/multinomial_naive_bayes.py
+++ pylearn/classification/multinomial_naive_bayes.py
@@ -3,14 +3,20 @@ import pandas as pd
 import time
 import re
 
-# TODO count word in one sentence once or count occurances
 class MultinomialNaiveBayes:
     """
     Computes text classification problems by applying the Bayes theorem.
 
-    Parameters:
-        :TODO
+    Attributes:
+        :texts (pandas.Series): Input texts used for training
+        :classes (list): List of unique classes in the training data
+        :num_of_samples (int): Total number of samples in the training data
+        :prior (pandas.DataFrame): Prior probabilities of each class
+        :vocab (list): List of unique words in the training data
+        :posterior (pandas.DataFrame): Posterior probabilities of each word given each class
     """
+    # loc selects rows by index label (can also be numeric, but numeric index can differ from real index), 
+    # iloc selects row by actual index
     def fit(self, X: pd.Series, Y: pd.Series, alpha=1, log_duration=True) -> None:
         """
         Trains the algorithm. 
@@ -30,35 +36,31 @@ class MultinomialNaiveBayes:
         self.texts = X
         self.classes = sorted(list(Y.unique()))
         self.num_of_samples = len(X) 
-        self.prior = pd.DataFrame(data=0, index=range(len(self.classes)), columns=range(1))
-        tokenized_texts = self._tokenize(self.texts)  # list of lists of words of one row
+        self.prior = pd.DataFrame(data=0, index=range(len(self.classes)), columns=range(1)).astype(float)
+        tokenized_texts = self._tokenize(self.texts)        # list of lists of words of one text
         self.vocab = self._create_vocab(tokenized_texts)    # contains all words
-        self.posterior = pd.DataFrame(data=0, index=range(len(self.classes)), columns=self.vocab)
+        self.posterior = pd.DataFrame(data=0, index=range(len(self.classes)), columns=self.vocab).astype(float)
         for index, c in enumerate(self.classes):
             class_df = X[Y == c]
-            print(class_df)
-            self.prior.iloc[index] = float(len(class_df)) / float(self.num_of_samples)
-            tokenized_class_texts = self._tokenize(class_df)
-            print(tokenized_class_texts)
+            self.prior.iloc[index] = len(class_df) / self.num_of_samples                # ratio of classes in the sample
+            tokenized_class_texts = self._tokenize(class_df)                            # tokenized texts per class
             self.posterior.iloc[index] = self._bow(tokenized_class_texts)         
-            print("before")
-            print(self.posterior)
             # Laplace smoothing:
-            # P(x|y) = (N_x,y + α) / (N_y + α * D)
+            # P(x|y) = (N_x,y + α) / (N_y + α * D)  
             # α: smoothing parameter
-            N_xy = self.posterior.iloc[index]           # N_x,y: number of x occuring in class y               
-            N_y = len(tokenized_class_texts[0])         # N_y: number of features in y
-            D = self.posterior.shape[1]                 # D: number of features in whole data set
+            N_xy = self.posterior.iloc[index]           # N_x,y: occuramce of feature (word) x in class y    
+            N_y = 0                                     # N_y: number of features (words) in y
+            for text in tokenized_class_texts:          
+                N_y += len(text)
+            D = self.posterior.shape[1]                 # D: number of features (words) in whole data set
             self.posterior.iloc[index] = (N_xy + alpha) / (N_y + alpha * D)
-            print("after")
-            print(self.posterior)
-
+            
         end = time.time()
 
         if log_duration:
             print(f"Duration of training: {end - start}\n")
     
-    def predict(self, X: pd.Series) -> pd.DataFrame:
+    def predict(self, X: pd.Series) -> np.ndarray:
         """
         Computes the output of a given X.
 
@@ -66,26 +68,36 @@ class MultinomialNaiveBayes:
             :X (pandas.Series): Testing input
 
         Returns:
-            Predicted classes as pandas dataframe
+            Predicted classes as array
         """
         y_pred = [self._predict(x[1:len(x)]) for x in X.items()]           # x is item object --> x[1:len(x)] removes index (iteritems deprecated)
-        return pd.DataFrame(y_pred)                                         # TODO change to Series for easier usage/transformation to array
-    
+        return np.array(y_pred)                                        
+
     def _predict(self, x: tuple) -> int | str:
-        #print(x)
-        x = self._tokenize(pd.Series(x))[0]             # get a list of all words
-        #print(x)
+        """
+        Helper function for predict.
+
+        Parameters:
+            :x (tuple): A tuple with the tokenized text
+
+        Returns:
+            The class with the highest probability
+        """
         # P(y) * ∏ P(x_i|y) --> ln P(y) + ∑ ln P(x_i|y) to prevent underflow
-        posterior = self.posterior[x].sum(axis=1) 
-        # Laplace-Smoothing to avoid log of 0, wrong yet
 
-        
-        #if posterior
-        for index, c in enumerate(self.classes):
-            prior = np.log(self.prior.iloc[index])       # get prior at index
-            posterior.iloc[index] = np.log(posterior.iloc[index]) + prior                
-        print(posterior)
-
+        x = self._tokenize(pd.Series(x))[0]             # get a list of all words
+        prior = np.log(self.prior)                      # ln P(y)
+        posterior = pd.DataFrame(data=0, index=range(len(self.classes)), columns=x).astype(float)
+        try:
+            posterior = np.log(self.posterior[x])       # ln P(x_i|y)
+        except KeyError:                                # self.posterior[x] raises KeyError if word not found, happens if vocab not big enough
+            for word in x:
+                if word not in self.posterior.columns:
+                    posterior[word] = 1e-10             # add new column (word) with probability near 0
+                else:
+                    posterior[word] = self.posterior[word]
+        posterior = posterior.sum(axis=1)               # ∑ ln P(x_i|y)
+        posterior = prior[0] + posterior                # ln P(y) + ∑ ln P(x_i|y)       (prior[0] to change DataFrame to Series)
         return self.classes[np.argmax(posterior)]
     
     def _bow(self, tokenized_texts: list) -> pd.DataFrame:
@@ -100,33 +112,25 @@ class MultinomialNaiveBayes:
             The probabilities of each word as pandas dataframe
         """
         bow_df = pd.DataFrame(np.zeros((len(tokenized_texts), len(self.vocab))), columns=self.vocab)
-        print(bow_df.shape)
-        print("tt", tokenized_texts)
+
         for i, text in enumerate(tokenized_texts):
             for word in text:
-                bow_df.at[i, word] = 1
-        # print("a")
-        # print(bow_df)
-        # for i, text in enumerate(tokenized_texts):
-        #     bow_df.loc[i, text] = 1
-        # print("b")
-        # print(bow_df)
-        # word_probability = {}
+                bow_df.at[i, word] += 1
 
-        # for column in bow_df.columns:
-        #     for word in bow_df[column]:
-        #         word_probability[column] = bow_df[column].sum() / self.num_of_samples     # TODO change to self.num_of_samples
-        print("bow df")
-        print(bow_df)
-        word_probability = bow_df.sum(axis=0) #/ self.num_of_samples
+        word_probability = bow_df.sum(axis=0) 
    
-        #print(bow_df.head())
         return pd.DataFrame([word_probability])    
         
     @staticmethod
     def _create_vocab(tokenized_texts: list) -> list:
         """
         Helper function for fit.
+
+        Parameters:
+            :tokenized_texts (list): A list of the words of each sample
+
+        Returns:
+            The vocabulary with all unique words
         """
         return list(set(word for text in tokenized_texts for word in text))
     
@@ -134,6 +138,12 @@ class MultinomialNaiveBayes:
     def _tokenize(texts: pd.Series) -> list:
         """
         Helper function for fit.
+
+        Parameters:
+            :texts (pandas.Series): All texts in a series
+
+        Returns:
+            The texts as a list of lists of tokenized texts, input is cleaned from special characters
         """
         def clean_text(text):
                 # remove everything but letters and whitespace (note: falsifies some words, e.g. Gerard's --> Gerards)
@@ -143,15 +153,3 @@ class MultinomialNaiveBayes:
 
         texts = texts.tolist()
         return [clean_text(text) for text in texts]
-        
-    
-
-if __name__ == "__main__":  
-    data = pd.read_csv("examples/data/fake_news.csv")
-    data = data[["title", "real"]]      # remove unneccessary columns
-    
-    nb = MultinomialNaiveBayes()
-    nb.fit(None, None)
-    # nb.fit(data["title"], data["real"])
-    # print(nb.predict(data["title"]))
-    
